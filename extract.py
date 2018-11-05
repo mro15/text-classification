f118ce58c0bb36aa6ea55e490af110d05f5aa708
Thiago Jorge Abdo <tja14@inf.ufpr.br>
2018-11-05 17:50:25 -0200

    Add baseExtractor and extract.py code(missing the part that saves features)
    Signed-off-by: Thiago Jorge Abdo <tja14@inf.ufpr.br>

---
+++ extract.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+import argparse
+import extractors
+from os import listdir
+from os.path import isfile, join
+
+from IPython import embed
+
+debug = False
+
+def read_args():
+    parser = argparse.ArgumentParser(description='Os parametros para os extratores são:')
+    parser.add_argument('--data', type=str, help='arquivo de data', required=True)
+    parser.add_argument('--extractor', type=str, help='Extrair com o seguinte extrator', required=True)
+    parser.add_argument('--debug', help='Print debug and load less files', default=False, action="store_true")
+    return parser.parse_args()
+
+def pprint(text):
+    if debug:
+        print(text)
+
+def read_imdb():
+    dataset = "datasets/imdb/aclImdb/"
+
+    dado = {}
+    dado["test"] = {}
+    dado["test"]["pos"] = []
+    dado["test"]["neg"] = []
+    dado["train"] = {}
+    dado["train"]["pos"] = []
+    dado["train"]["neg"] = []
+
+    for i in ["test", "train"]:
+        for j in ["pos", "neg"]:
+            files = listdir(dataset+i+"/"+j+"/")
+            for f in files:
+                fp = open(dataset+i+"/"+j+"/"+f, 'r')
+                dado[i][j].append(fp.readlines())
+                if debug and len(dado[i][j]) == 5:
+                    break
+
+    train = dado["train"]["pos"] + dado["train"]["neg"]
+    label_train = [1]*len(dado["train"]["pos"]) + [0]*len(dado["train"]["neg"])
+
+    test = dado["test"]["pos"] + dado["test"]["neg"]
+    label_test = [1]*len(dado["test"]["pos"]) + [0]*len(dado["test"]["neg"])
+
+    return [train, label_train], [test, label_test]
+
+def read_polarity():
+    dataset = "datasets/polarity/txt_sentoken/"
+
+    dado = {}
+    dado["neg"] = []
+    dado["pos"] = []
+
+    for i in ["neg", "pos"]:
+        files = listdir(dataset+i+"/")
+        for f in files:
+            fp = open(dataset+i+"/"+f, 'r')
+            dado[i].append(fp.readlines())
+            if debug and len(dado[i]) == 10:
+                break;
+
+    train_neg = dado["neg"][:int(len(dado["neg"])/2)]
+    train_pos = dado["pos"][:int(len(dado["pos"])/2)]
+    train = train_pos + train_neg
+    label_train = [1]*len(train_pos) + [0]*len(train_neg)
+
+    test_neg = dado["neg"][int(len(dado["neg"])/2):]
+    test_pos = dado["pos"][int(len(dado["pos"])/2):]
+    test = test_pos + test_neg
+    label_test = [1]*len(test_pos) + [0]*len(test_neg)
+
+    return [train, label_train], [test, label_test]
+
+def main():
+    opt = read_args()
+    global debug
+    debug = opt.debug
+
+    if opt.data == "imdb":
+        train,test = read_imdb()
+    elif opt.data == "polarity":
+        train,test = read_polarity()
+    else:
+        print("Erro, database nao identificado")
+        return 1
+
+    #Possibel extractors
+    PE = {}
+    PE["w2v"] = extractor.W2v
+
+    extract = PE[opt.extractor]
+    extract(train,test,name=opt.extractor)
+    extract.init()
+    extract.model1_init()
+    extract.run()
+
+if __name__ == "__main__":
+    main()
---
+++ extractors/baseExtractor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Extractor(object):
+    def __init__(self, train_data, test_data, name = ""):
+        self.train_data = train_data
+        self.test_data = test_data
+        self.name = name
+
+    def run(self):
+        pass
+
