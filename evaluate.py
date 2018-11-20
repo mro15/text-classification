b7d569134e8bd618d9e023347f64698f32366600
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2018-11-20 11:45:42 -0200

    evaluate now support all predicts and best predict

--- evaluate.py
+++ evaluate.py
@@ -11,6 +11,7 @@ debug = False
 def read_args():
     parser = argparse.ArgumentParser(description='Os parametros para os extratores são:')
     parser.add_argument('--file', type=str, help='arquivo com as predicoes', required=True)
+    parser.add_argument('--mode', type=str, help='comparar 10 ou calcular apenas best [all, best]', required=True)
     parser.add_argument('--debug', help='Print debug and load less files', default=False, action="store_true")
     return parser.parse_args()
 
@@ -18,7 +19,7 @@ def pprint(text):
     if debug:
         print(text)
 
-def read_results(file_name):
+def read_best(file_name):
     y_test = []
     y_pred = []
     with open(file_name, 'r') as f:
@@ -29,14 +30,43 @@ def read_results(file_name):
             y_pred.append(line[-1])
     return y_test, y_pred
 
+def read_all(file_name):
+    y_test_all = []
+    y_pred_all = []
+    for i in range(0, 10):
+        y_test = []
+        y_pred = []
+        with open(file_name+str(i), 'r') as f:
+            result = csv.reader(f)
+            #deve ter um jeito melhor de fazer isso xP
+            for line in result:
+                y_test.append(line[0])
+                y_pred.append(line[-1])
+        y_test_all.append(y_test)
+        y_pred_all.append(y_pred)
+    return y_test_all, y_pred_all
+
+
 def main():
     opt = read_args()
     global debug
     debug = opt.debug
-    y_test, y_pred = read_results(opt.file)
-    print(accuracy_score(y_test, y_pred))
-    print(f1_score(y_test, y_pred, average='macro'))
-    print(confusion_matrix(y_test, y_pred))
+    if opt.mode == "best":
+        y_test, y_pred = read_best(opt.file)
+        print(accuracy_score(y_test, y_pred))
+        print(f1_score(y_test, y_pred, average='macro'))
+        print(confusion_matrix(y_test, y_pred))
+    elif opt.mode == "all":
+        y_test, y_pred = read_all(opt.file)
+        acc = 0
+        f1 = 0
+        for i in range(0, 10):
+            acc+=accuracy_score(y_test[i], y_pred[i])
+            f1+=f1_score(y_test[i], y_pred[i], average='macro')
+        print("Score medio: " + str(acc/10))
+        print("F1 medio: " + str(f1/10))
+    else:
+        print("Modo desconhecido")
 
 if __name__ == "__main__":
     main()
